d59f92cdc09321f32f6c57d15b341ed4a117ad29
abstract-thoery <none>
2020-03-12 19:37:46 -0700

    Fix some issues with management commands.

--- ipshield/management/commands/unblock.py
+++ ipshield/management/commands/unblock.py
@@ -7,59 +7,60 @@ class Command(BaseCommand):
     ======
         Listing blocks and logs
         -----------------------
-        python manage.py unblock --listblocks
-        python manage.py unblock --listlogs
-        python manage.py unblock --list
+        django-admin unblock --list
+        django-admin unblock --log
+        django-admin unblock --block
 
         Removing blocks and logs
         ------------------------
-        python manage.py unblock all
-        python manage.py unblock --ips 127.0.0.1
-        python manage.py unblock --events event_name
+        django-admin unblock --rmip <IP_Address>
+        django-admin unblock --rmevent <Event_Name>
+        django-admin unblock --rmall
     """
 
-    help = 'Unblocks IP addresses.'
+    help = 'Django management commands for django-ip-shield.'
 
 
     def add_arguments(self, parser):
 
         parser.add_argument(
-            '--stat',
+            '--list',
             action='store_true',
-            help='Display number of logged events and blocked IPs+events.',
+            help='List number of logged events and blocked IPs+events.',
         )
 
         parser.add_argument(
-            '--blocks',
+            '--block',
             action='store_true',
-            help='Display list of all blocked IPS+events.',
+            help='List of all blocked IPS+events.',
         )
 
         parser.add_argument(
-            '--logs',
+            '--log',
             action='store_true',
-            help='Display list of all logged events.',
+            help='List of all logged events.',
         )
 
         parser.add_argument(
-            '--ips',
-            nargs='?',
+            '--rmip',
+            nargs='+',
             action='append',
-            help='Delete logs and blocks for listed IP addresses.',
+            help='Remove logs and blocks for listed IP addresses.',
         )
 
         parser.add_argument(
-            '--events',
-            nargs='?',
+            '--rmevent',
+            nargs='+',
             action='append',
-            help='Delete logs and blocks for listed events.',
+            help='Remove logs and blocks for listed events.',
         )
 
         parser.add_argument(
-            '--all',
+            '--rmall',
             action='store_true',
-            help='Delete all logs and blocks.',
+            help='Remove all logs and blocks.',
         )
+
 
     def success(self, msg):
         self.stdout.write(self.style.SUCCESS(msg))
@@ -67,45 +68,47 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        if options['stat']:
+        if options['list']:
             l = Log.objects.all()
             b = Blocked.objects.all()
             self.success(' %d events in log.' % l.count())
             self.success(' %d IP+events in block list.' % b.count())
 
-        elif options['blocks']:
+        elif options['block']:
             self.success('Blocked IPs + Events:')
             b = Blocked.objects.all()
             for B in b.values():
                 print('    "%s", "%s", "%s"' % (B.get('IpAddress'), B.get('EventName'), B.get('BlockDate')))
 
-        elif options['logs']:
+        elif options['log']:
             self.success('Logged events:')
             l = Log.objects.all()
             for L in l.values():
                 print('    "%s", "%s", "%s"' % (L.get('IpAddress'), L.get('EventName'), L.get('EventDate')))
 
-        elif options['ips']:
-            for ipAddress in options['ips']:
-                self.success('IP Address "%s":' % ipAddress)
-                l = Log.objects.filter(IpAddress=ipAddress)
-                b = Blocked.objects.filter(IpAddress=ipAddress)
-                self.success('    %d logged events removed.' % l.count())
-                self.success('    %d blocked events removed.' % b.count())
-                l.delete()
-                b.delete()
+        elif options['rmip']:
+            for addresses in options['rmip']:
+                for ip in addresses:
+                    self.success('IP Address "%s":' % ip)
+                    l = Log.objects.filter(IpAddress=ip)
+                    b = Blocked.objects.filter(IpAddress=ip)
+                    self.success('    %d logged events removed.' % l.count())
+                    self.success('    %d blocked events removed.' % b.count())
+                    l.delete()
+                    b.delete()
 
-        elif options['events']:
-            for eventName in options['events']:
-                self.success('Event name "%s":' % eventName)
-                l = Log.objects.filter(EventName=eventName)
-                b = Blocked.objects.filter(EventName=eventName)
-                self.success('    %d logged IPs removed.' % l.count())
-                self.success('    %d blocked IPs removed.' % b.count())
-                l.delete()
-                b.delete()
+        elif options['rmevent']:
+            for events in options['rmevent']:
+                for ev in events:
+                    self.success('Event name "%s":' % ev)
+                    l = Log.objects.filter(EventName=ev)
+                    b = Blocked.objects.filter(EventName=ev)
+                    self.success('    %d logged IPs removed.' % l.count())
+                    self.success('    %d blocked IPs removed.' % b.count())
+                    l.delete()
+                    b.delete()
 
-        elif options['all']:
+        elif options['rmall']:
             l = Log.objects.all()
             b = Blocked.objects.all()
             self.success('Removing %d entries from log.' % l.count())
@@ -118,27 +121,4 @@ class Command(BaseCommand):
 
 
 
-        # # To do:
-        # #   display some more data
-        # #
-        # elif options['ips']:
-            # for ipAddress in options['ips']:
-                # self.success('IP Address "%s":' % ipAddress)
-                # l = Log.objects.filter(IpAddress=ipAddress)
-                # b = Blocked.objects.filter(IpAddress=ipAddress)
-
-                # ld = l.values_list('EventName', flat=True).distinct()
-                # ls = list(ld) or None
-
-                # bd = b.values_list('EventName', flat=True).distinct()
-                # bs = list(bd) or None
-
-                # self.success('    Events found: %s' % ls)
-                # self.success('    %d logged events removed.' % l.count())
-
-                # self.success('    Blocks found: %s' % bs)
-                # self.success('    %d blocked events removed.' % b.count())
-
-                # l.delete()
-                # b.delete()
 
