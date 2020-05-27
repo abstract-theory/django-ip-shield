92739d3d0ac334c87a2451e29d8a3ca044f9a491
abstract-thoery <none>
2020-05-27 13:14:15 -0700

    Update system requirements.

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
+        'Framework :: Django :: 1.9',
         'Framework :: Django :: 1.10',
         'Framework :: Django :: 1.11',
         'Framework :: Django :: 2.0',
